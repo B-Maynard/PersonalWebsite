338ecba9ebcd1ad5c3289ab950d136d41a1c78c8
Bailey Maynard <bailey_maynard@live.com>
2022-05-25 00:12:08 -0400

    updates

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -4,3 +4,21 @@ function fadeOutSplashScreen() {
     $("#home-screen").fadeIn();
   }, 1000);
 }
+
+function transitionCard(newCard, newTab) {
+  // when a new item is clicked on left side, we fade out current card and fade in new one
+  var currentTab = $(".list-group-item-dark");
+  var currentCard = $(".active-card");
+
+  currentTab.removeClass("list-group-item-dark");
+  currentTab.addClass("list-group-item");
+
+  currentCard.fadeOut();
+  currentCard.removeClass(".active-card");
+  
+  $(newTab).addClass("list-group-item-dark");
+  setTimeout(function() {
+    $(newCard).fadeIn();
+    $(newCard).addClass(".active-card")
+  }, 1000);
+}
